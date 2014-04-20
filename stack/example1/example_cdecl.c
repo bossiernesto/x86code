c0a1c3ebc445da04a8bea56bcfef26611a2918b2
ernesto <bossi.ernestog@gmail.com>
2014-04-20 11:23:49 -0300

    initial stack examples dissasembled

---
+++ stack/example1/example_cdecl.c
@@ -0,0 +1,12 @@
+//Example1 - using the stack 
+////to call subroutines
+////New instructions: 
+////push, pop, call, ret, mov
+int sub(){
+    return 0xbeef;
+}
+
+int main(){
+    sub();
+    return 0xf00d;
+}
